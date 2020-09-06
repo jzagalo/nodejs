dfed23b7b5eb26b4522c896b98791a9e84211e79
Paul Ezaga <paulezaga@gmail.com>
2020-09-06 19:32:10 +0200

    Adding Caching to Files

--- server.js
+++ server.js
@@ -15,12 +15,43 @@ var mimeTypes = {
     '.css' : 'text/css'
 };
 
+var cache = {};
+function cacheAndDeliver(f, cb){
+    if(!cache[f]){
+        fs.readFile(f, function(err,data){
+            if(!err){
+                cache[f] = { content: data };
+            }
+
+            cb(err, data);
+        });        
+        return;
+    }
+    console.log('loading ' + f + ' from cache');
+    cb(null, cache[f].content);
+}
+
 http.createServer(function(req, res){    
      var lookup = path.basename(decodeURI(req.url)) || 'index.html';
      var f = './' + lookup;
 
-     fs.exists(f, function(exists){
-         console.log(exists ? lookup + " is there" : lookup + " doesn't exsit");
+     fs.exists(f, function(exists){               
+        if(exists){
+            cacheAndDeliver(f, function(err, data){
+                if(err) { 
+                    res.writeHead(500);
+                    res.end("Server Error!");
+                    return;
+                }
+                var headers = { 'Content-type': mimeTypes[path.extname(lookup)]};
+                res.writeHead(200, headers);
+                res.end(data);
+            });
+            return;
+        }
+        res.writeHead(404); // no such file found
+        res.end()
      })
+     console.log(cache);
     
 }).listen(8080);
