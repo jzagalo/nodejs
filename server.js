76f592be7788cfd1288a0386bd4afb01d010f0f6
Paul Ezaga <paulezaga@gmail.com>
2020-09-07 18:56:04 +0200

    NodeJS fileUpload with PUT and XHR

--- server.js
+++ server.js
@@ -7,45 +7,31 @@ var url = require('url');
 var fs = require('fs');
 var util = require('util');
 var querystring = require('querystring');
-var form = require('fs').readFileSync('form.html');
+var form = require('fs').readFileSync('put_upload_form.html');
 var maxData = 2 * 1024 * 1024;
 
-var pages = [
- { id: '1', route: '', output: 'Woohoo!'},
- { id: '2', route: 'about', output: 'A simple routing with Node example'},
- { id: '3', route: 'another-page', output: function() {return "Here\'s " + this.route; }},
-];
 
-var mimeTypes = {
-    '.js' : 'text/javascript',
-    '.html' : 'text/html',
-    '.css' : 'text/css'
-};
 
 http.createServer(function(request, response){
-    if(request.method === "POST") {
-        var incoming = formidable({ multiples: true, uploadDir: 'uploads' }); 
-
-        incoming.on('fileBegin', function(field, file){
-            if(file.name){
-                file.path += '-' + file.name
-                console.log(file.path);
-            }
-        })
-        .on('file', function(field, file){
-            if(! file.size)  return;            
-            response.write(file.name + ' received\n');
-        }).on('field', function(field, value){
-            response.write(field + ' : ' + value + '\n' );
+    if(request.method === "PUT"){
+        var fileData = new Buffer(+request.headers['content-length']);
+        var bufferOffset= 0;
+        request.on('data', function(chunk){
+            chunk.copy(fileData, bufferOffset);
+            bufferOffset += chunk.length;
         }).on('end', function(){
-            response.end('All files received');
+            var rand = (Math.random()* Math.random()).toString(16).replace('.', '');
+            var to = 'uploads/' + rand + "-" + request.headers['x-uploadedfilename'];
+
+            fs.writeFile(to, fileData, function(err){
+                if(err) { throw err; }
+                console.log('Saved file to ' + to );
+                response.end();
+            });
         });
-
-        incoming.parse(request);
     }
-
-    if(request.method === "GET") {        
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end(form)
+    if(request.method === "GET"){
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(form);
     }
 }).listen(8080);
