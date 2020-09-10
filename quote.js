bee01e23fc6bc116ff69d604c906904c41178d7e
Paul Ezaga <paulezaga@gmail.com>
2020-09-10 12:58:12 +0200

    Moved quotes to another file

--- quote.js
+++ quote.js
@@ -5,7 +5,8 @@ client.connect('mongodb://127.0.0.1:27017', function(err, client) {
     
     var db = client.db('admin');
     if(err) throw err;
-    var collection = db.collection('quotes');
+    var collection = db.collection('quotes');    
+
     if(params.author && params.quote){
         collection.insertOne({
             author: params.author,
