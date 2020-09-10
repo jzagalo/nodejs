9a85145f111cb0d0511414eac3bdfe24ff2041f0
Paul Ezaga <paulezaga@gmail.com>
2020-09-10 18:19:12 +0200

    Getting Started with Websockets

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
+var WSServer = require('ws').Server;
 var http = require('http');
-var path = require('path');
-var connect = require('connect');
-var formidable = require('formidable');
-var url = require('url');
 var fs = require('fs');
-var util = require('util');
-var querystring = require('querystring');
-var form = require('fs').readFileSync('put_upload_form.html');
+var index = fs.readFileSync('./client.html');
 
-http.createServer(function(request, response){
-    if(request.method === "PUT"){
-        var fileData = new Buffer(+request.headers['content-length']);
-        var bufferOffset= 0;
-        request.on('data', function(chunk){
-            chunk.copy(fileData, bufferOffset);
-            bufferOffset += chunk.length;
-        }).on('end', function(){
-            var rand = (Math.random()* Math.random()).toString(16).replace('.', '');
-            var to = 'uploads/' + rand + "-" + request.headers['x-uploadedfilename'];
 
-            fs.writeFile(to, fileData, function(err){
-                if(err) { throw err; }
-                console.log('Saved file to ' + to );
-                response.end();
-            });
-        });
-    }
-    if(request.method === "GET"){
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(form);
-    }
-}).listen(8080);
+var server = http.createServer("/", function(req, res){     
+    res.end(index);
+});
+
+var wss = new WSServer({ server });
+console.log(wss);
+wss.on('connection', function(socket, req) { 
+    socket.on('message', function(msg){
+        console.log('Recieved: ', msg, '\n', 'From IP: ',  req.socket.remoteAddress);
+
+        if(msg === 'Hello'){
+            socket.send("Websockets!");
+        }
+    });
+
+    socket.on('close', function(code, desc){
+        console.log('Disconnect: ' + code + ' - ' + desc);
+    });   
+     
+});
+
+server.listen(3000);
