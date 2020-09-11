1fa895b0c44ed8a9f2a3236024b02762a9d9c0e0
Paul Ezaga <paulezaga@gmail.com>
2020-09-11 12:52:47 +0100

    SocketIO web sockets implementation

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var clientHtml = require('fs').readFileSync('./client.html');
 var plainHttpServer = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-type': 'text/html'});
     res.end(clientHtml);
-}).listen(8080);
+}).listen(3000);
 
 var io = require('socket.io').listen(plainHttpServer);
 io.sockets.on('connection', function(socket){
-    socket.on('message', function(msg){        
-        if(msg === "Hello"){            
+    socket.on('message', function(msg){
+        if(msg === "Hello"){
             socket.send('socket.io!');
         }
     })
